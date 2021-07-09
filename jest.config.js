1486a3557d540c9f2bcb4477f23f4336c3789f16
王刚 <wanggang@tellyes.com>
2021-07-09 17:29:40 +0800

    fix: 修改单元测试用例

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   moduleFileExtensions: ['js', 'ts', 'tsx', 'vue'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': 'vue-jest',
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\js$': 'babel-jest',
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  testMatch: ['<rootDir>/tests/**/*.(spec|test).(ts|tsx)|**/__tests__/*.(ts|tsx)']
+  testMatch: ['<rootDir>/tests/**/*.(spec|test).(ts|tsx)|**/__tests__/*.(ts|tsx)'],
 }
